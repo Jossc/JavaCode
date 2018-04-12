dbaeb5f2438697b5f4d871278c88e4e03c94dc74
chenzhuo@wanhuchina.com <chenzhuo@wanhuchina.com>
2018-04-12 09:03:16 +0800

    basics code  for cz threadLocal

---
+++ src/main/java/com/basics/ThreadCode/ThreadLocalDemo/ThreadLocalTest.java
@@ -0,0 +1,35 @@
+package com.basics.ThreadCode.ThreadLocalDemo;
+
+/**
+ * @PACKAGE_NAME: com.basics.ThreadCode.ThreadLocalDemo
+ * @PROJECT_NAME: JavaCode
+ * @Date: 2018/4/11
+ * @author: chenzhuo
+ * 本地线程变量
+ * 内部 ThreadLocalMap
+ */
+public class ThreadLocalTest {
+    static  ThreadLocal<String> stringThreadLocal = new ThreadLocal<>();
+    static void print(String str){
+        System.out.println(str + ": " + stringThreadLocal.get());
+    }
+
+    public static  void main(String []args){
+        Thread thread = new Thread(()->{
+            stringThreadLocal.set("thread on threadLocal one");
+            print("threadOne");
+            System.out.println("threadOne " + stringThreadLocal.get());
+        });
+
+        Thread thread1 = new Thread(()->{
+            stringThreadLocal.set("thread on threadLocal two");
+            print("threadTwo");
+            System.out.println("threadTwo " + ":" +stringThreadLocal.get());
+        });
+
+        thread.start();
+        thread1.start();
+
+
+    }
+}
