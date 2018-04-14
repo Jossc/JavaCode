6085699b0fa1b761547f94c0b64368b649c4a2f8
Clement <czhuo02@163.com>
2018-04-14 22:56:05 +0800

    queue

--- src/main/java/com/basics/queue/QueueQu/TestConcurrentLinkedQueue.java
+++ src/main/java/com/basics/queue/QueueQu/TestConcurrentLinkedQueue.java
@@ -1,11 +1,80 @@
 package com.basics.queue.QueueQu;
 
+import com.basics.queue.utils.User;
+
+import java.util.concurrent.*;
+
 /**
  * @PACKAGE_NAME: com.basics.queue.QueueQu
  * @PROJECT_NAME: JavaCode
  * @Date: 2018/4/10
  * @author: chenzhuo
- * v无界非阻塞队列ConcurrentLinkedQueue
+ * 无界非阻塞队列ConcurrentLinkedQueue
  */
 public class TestConcurrentLinkedQueue {
+
+    private static ConcurrentLinkedDeque<User> concurrentLinkedDeque
+            = new ConcurrentLinkedDeque<User>();
+
+    private static ExecutorService executorService = Executors.newScheduledThreadPool(3);
+
+
+    public static void main(String []args){
+
+        User user = new User("124",11);
+        User user1 = new User("234",123);
+        User user2 = new User("44",22);
+        User user3 = new User("55",11);
+
+    /*    concurrentLinkedDeque.add(user);
+        concurrentLinkedDeque.addLast(user1);
+        concurrentLinkedDeque.add(user1);
+        concurrentLinkedDeque.add(user2);
+        concurrentLinkedDeque.add(user3);
+
+        concurrentLinkedDeque.forEach(u->
+        System.err.println("username :"
+            + u.getUserName())
+        );*/
+/*
+        ll:
+        for(int i=0;i<100;i++){
+            if(i==1){
+                System.err.println(i +" :i");
+                break ll;
+            }
+        }
+*/
+
+        executorService.submit(()->{
+            concurrentLinkedDeque.add(user);
+            concurrentLinkedDeque.add(user1);
+            concurrentLinkedDeque.add(user2);
+            concurrentLinkedDeque.add(user3);
+        });
+   /*     executorService.submit(()->{
+            concurrentLinkedDeque.add(user);
+            concurrentLinkedDeque.add(user1);
+            concurrentLinkedDeque.add(user2);
+            concurrentLinkedDeque.add(user3);
+        });
+        executorService.submit(()->{
+            concurrentLinkedDeque.add(user);
+            concurrentLinkedDeque.add(user1);
+            concurrentLinkedDeque.add(user2);
+            concurrentLinkedDeque.add(user3);
+        });
+*/
+        concurrentLinkedDeque.forEach(u->
+                System.err.println("username :"
+                        + u.getUserName())
+        );
+
+        executorService.shutdownNow();
+
+
+
+
+    }
+
 }
