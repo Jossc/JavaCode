69cf40b62256426e0b337539afbd162dab879b55
chenzhuo@wanhuchina.com <chenzhuo@wanhuchina.com>
2018-04-19 14:11:04 +0800

    basics code  for cz Unsafe

---
+++ src/main/java/com/basics/UnsafeTest/UnsafeTestOne.java
@@ -0,0 +1,35 @@
+package com.basics.UnsafeTest;
+
+import sun.misc.Unsafe;
+
+/**
+ * @PACKAGE_NAME: com.basics.UnsafeTest
+ * @PROJECT_NAME: JavaCode
+ * @Date: 2018/4/19
+ * @author: Jossc
+ * 因为Unsafe是Bootstrap 类加载的
+ * 但是UnsafeTestOne是AppClassLoader类加载器加载的
+ * 所以出现java.lang.SecurityException: Unsafe异常
+ */
+public class UnsafeTestOne {
+    private static Unsafe unsafe = Unsafe.getUnsafe();
+    private static  long stateOffset;
+    private static volatile  long state = 0 ;
+    static {
+        try {
+            stateOffset = unsafe.objectFieldOffset(UnsafeTestOne.class
+            .getDeclaredField("state"));
+        } catch (NoSuchFieldException e) {
+            System.out.println(e.getLocalizedMessage());
+            throw new Error(e);
+        }
+    }
+    public static void main(String []args ){
+        UnsafeTestOne testOne = new UnsafeTestOne();
+        Boolean b = unsafe.compareAndSwapInt(testOne,stateOffset,0,1);
+        System.out.println(" sucess : "+ b);
+    }
+
+}
+
+
---
+++ src/main/java/com/basics/UnsafeTest/UnsafeTestTwo.java
@@ -0,0 +1,41 @@
+package com.basics.UnsafeTest;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @PACKAGE_NAME: com.basics.UnsafeTest
+ * @PROJECT_NAME: JavaCode
+ * @Date: 2018/4/19
+ * @author: Jossc
+ * 黑科技反射获取Unsafe
+ */
+public class UnsafeTestTwo {
+    private  static final Unsafe unsafe ;
+
+    private  static final long stateOffset;
+    private  volatile  long state = 0;
+
+    static {
+        try{
+             Field field = Unsafe.class.getDeclaredField("theUnsafe");
+             field.setAccessible(true);
+             unsafe = (Unsafe) field.get(null);
+            stateOffset =
+                    unsafe.objectFieldOffset(UnsafeTestTwo.class.
+                            getDeclaredField("state"));
+            System.out.println("stateOffset: " +stateOffset);
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+            throw new Error(e);
+        }
+    }
+
+    public static  void  main(String []args){
+        UnsafeTestTwo unsafeTestTwo = new UnsafeTestTwo();
+        Boolean b  = unsafe.compareAndSwapInt(unsafeTestTwo,stateOffset,0,1);
+        System.out.println("success : " + b);
+
+    }
+}
