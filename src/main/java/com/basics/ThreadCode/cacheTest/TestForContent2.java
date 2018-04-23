fae1b51e5609f95628163408cd4e94d4cf8d8b4a
Jossc <czhuo02@163.com>
2018-04-23 13:56:09 +0800

    basics code  for cz

---
+++ src/main/java/com/basics/ThreadCode/cacheTest/TestForContent2.java
@@ -0,0 +1,30 @@
+package com.basics.ThreadCode.cacheTest;
+
+/**
+ * @PACKAGE_NAME: com.basics.ThreadCode.cacheTest
+ * @PROJECT_NAME: JavaCode
+ * @Date: 2018/4/23
+ * @author: Jossc
+ */
+public class TestForContent2 {
+    static  final int Line_Num = 1024;
+    static  final int Colum_Num = 1024;
+
+    /**
+     *  no cache time : 3
+     * @param args
+     */
+    public static void main(String []args){
+        long[][] array = new long[Line_Num][Colum_Num];
+        long startTime = System.currentTimeMillis();
+        for(int i= 0; i < Line_Num;i++){
+            for (int j=0;j<Colum_Num;++j){
+                array[i][j] = i *2+j;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        long cacheTime = endTime - startTime;
+        System.out.println("no cache time : " + cacheTime);
+
+    }
+}
