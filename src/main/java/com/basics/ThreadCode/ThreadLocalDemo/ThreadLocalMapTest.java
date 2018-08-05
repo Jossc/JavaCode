72d376a03101e4205657a91f874324280b44e2fd
Clement <czhuo02@163.com>
2018-08-05 22:07:40 +0800

    AtomicLong

---
+++ src/main/java/com/basics/ThreadCode/ThreadLocalDemo/ThreadLocalMapTest.java
@@ -0,0 +1,10 @@
+package com.basics.ThreadCode.ThreadLocalDemo;
+
+/**
+ * PACKAGE_NAME: com.basics.ThreadCode.ThreadLocalDemo
+ * MONTH_NAME_SHORT: 八月
+ * USER: Clement
+ **/
+public class ThreadLocalMapTest{
+
+}
--- src/main/java/com/basics/ThreadCode/ThreadLocalDemo/ThreadLocalTest.java
+++ src/main/java/com/basics/ThreadCode/ThreadLocalDemo/ThreadLocalTest.java
@@ -29,7 +29,7 @@ public class ThreadLocalTest {
 
         thread.start();
         thread1.start();
-
-
     }
+
+
 }
