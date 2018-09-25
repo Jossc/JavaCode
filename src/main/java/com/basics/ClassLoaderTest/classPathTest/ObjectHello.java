b6ac24c8682170d332c79e13333313629998fe6a
Clement <czhuo02@163.com>
2018-09-25 22:26:30 +0800

    Yeah, we the ones, who have the power。

---
+++ src/main/java/com/basics/ClassLoaderTest/classPathTest/ObjectHello.java
@@ -0,0 +1,13 @@
+package com.basics.ClassLoaderTest.classPathTest;
+
+/**
+ * PACKAGE_NAME: com.basics.ClassLoaderTest.classPathTest
+ * MONTH_NAME_SHORT: 九月
+ * USER: Clement
+ **/
+public class ObjectHello {
+    @Override
+    public String toString() {
+        return "我是小胖呀";
+    }
+}
