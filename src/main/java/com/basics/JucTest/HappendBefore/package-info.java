050eaa587c2c0fd384549b0713d7959e044056ea
Clement <czhuo02@163.com>
2018-10-08 23:04:19 +0800

    岁月难得沉默秋风厌倦漂泊 夕阳赖着不走挂在墙头舍不得我

--- src/main/java/com/basics/JucTest/HappendBefore/package-info.java
+++ src/main/java/com/basics/JucTest/HappendBefore/package-info.java
@@ -3,7 +3,17 @@
  * MONTH_NAME_SHORT: 十月
  * USER: Clement
  * happens before 原则
+ * as-if-serial 语义:在单线程的情况下无论怎么重排序 结果都不会变。
  *
+ * happens before 原则:
+ * 1.
+ * 2.
+ * 3.
+ * 4.
+ * 5.
+ * 6.
+ * 7.
+ * 8.
  *
  **/
 package com.basics.JucTest.HappendBefore;
