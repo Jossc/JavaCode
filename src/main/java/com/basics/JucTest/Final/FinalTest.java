bed935cb55e2ab24825c11d1a5beb609687543d4
Jossc <czhuo02@163.com>
2018-10-08 11:46:35 +0800

    没给你 我一生有愧 假如我年少有为 知进退 才不会让你替我受罪

--- src/main/java/com/basics/JucTest/Final/FinalTest.java
+++ src/main/java/com/basics/JucTest/Final/FinalTest.java
@@ -30,12 +30,8 @@ public class FinalTest {
      * 1.初次读取 finalTest
      * 2.初读普通变量i
      * 3.初读final变量j
-     *
      * final  在读之前jmm添加了load_load指令 防止重排序,所以
      * 普通变量i并不会被推到内存中(store_load才会)
-     *
-     *
-     *
      */
     public static  void read(){
         FinalTest finalTest1 = finalTest;
