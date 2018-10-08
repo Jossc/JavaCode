147823108609a038a92f4959c7f355982227b51e
Jossc <czhuo02@163.com>
2018-10-08 11:32:05 +0800

    final 语义

--- src/main/java/com/basics/JucTest/Final/FinalTest.java
+++ src/main/java/com/basics/JucTest/Final/FinalTest.java
@@ -24,6 +24,19 @@ public class FinalTest {
     public static void wirte(){
         finalTest = new FinalTest();
     }
+
+    /**
+     * 三个操作
+     * 1.初次读取 finalTest
+     * 2.初读普通变量i
+     * 3.初读final变量j
+     *
+     * final  在读之前jmm添加了load_load指令 防止重排序,所以
+     * 普通变量i并不会被推到内存中(store_load才会)
+     *
+     *
+     *
+     */
     public static  void read(){
         FinalTest finalTest1 = finalTest;
         int a = finalTest1.i;
