e3543eaefbfca01ad6655c748989717d616f3025
chenzhuo@picooc.com <chenzhuo>
2019-04-11 21:19:37 +0800

    1.并发包学习

---
+++ src/main/java/com/concurrent/cyclicBarrierLearn/CyclicBarrierLearnOne.java
@@ -0,0 +1,35 @@
+package com.concurrent.cyclicBarrierLearn;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @ClassName CyclicBarrierLearnOne
+ * @Despacito TODO
+ * @Author chenzhuo
+ * @Version 1.0
+ **/
+public class CyclicBarrierLearnOne {
+
+    private  static CyclicBarrier barrier;
+
+    public static void main(String[] args) {
+        barrier = new CyclicBarrier(3, () -> System.out.println("开始~~~"));
+        for(int i =0;i<3;i++){
+            new CyRun().start();
+        }
+    }
+
+    public static class CyRun extends  Thread{
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + "到了");
+            try {
+                barrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
