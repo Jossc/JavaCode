aa375799442e70c418f334e3814121b9ec9b5375
chenzhuo <chenzhuo@ybm.100.com>
2020-06-24 11:52:15 +0800

    1.添加netty学习

---
+++ src/main/java/netty/java/nio/ByteBufferTest.java
@@ -0,0 +1,30 @@
+package netty.java.nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author crazy
+ * @title: ByteBufferTest
+ * @projectName JavaCode
+ * @description: TODO
+ * @date 2020/6/2311:46
+ */
+public class ByteBufferTest {
+
+    public static void main(String[] args) {
+        /*ByteBuffer byteBuffer = ByteBuffer.allocate();*/
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        byte[] bytes = new byte[10];
+        bytes[0] = 1;
+        byteBuffer.put(bytes);
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        byteBuffer.limit(100);
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        System.out.println(byteBuffer.mark());
+        System.out.println(byteBuffer.remaining());
+
+
+    }
+}
---
+++ src/main/java/netty/java/nio/TimeClient.java
@@ -0,0 +1,22 @@
+package netty.java.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author crazy
+ * @title: TimeClient
+ * @projectName JavaCode
+ * @description:
+ * @date 2020/6/2310:50
+ */
+public class TimeClient {
+
+    public static void main(String[] args) throws IOException {
+       /* SocketAddress address = new InetSocketAddress();
+        SocketChannel socketChannel = SocketChannel.open();*/
+
+    }
+}
