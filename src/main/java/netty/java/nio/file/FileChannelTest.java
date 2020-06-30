405b696ff1e12ce57bc02b2192b16c9801b1c06c
chenzhuo <chenzhuo@ybm.100.com>
2020-06-30 18:07:24 +0800

    1.添加netty学习

---
+++ src/main/java/netty/java/nio/file/FileChannelTest.java
@@ -0,0 +1,40 @@
+package netty.java.nio.file;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+
+/**
+ * @author crazy
+ * @title: FileChannelTest
+ * @projectName JavaCode
+ * @description: TODO
+ * @date 2020/6/2419:52
+ */
+public class FileChannelTest {
+
+    public static void main(String[] args) throws Exception {
+        RandomAccessFile randomAccessFile
+                = new RandomAccessFile("/Users/crazy/Downloads/陈卓后端简历.pdf", "rws");
+        FileChannel channel = randomAccessFile.getChannel();
+        ByteBuffer buf = ByteBuffer.allocate(48);
+        ByteBuffer buffer = ByteBuffer.allocate(48);
+        ByteBuffer[] bufferArray = {buf, buffer};
+        long read = channel.read(bufferArray);
+        while (read != -1) {
+            System.out.println("Read " + bufferArray);
+            buf.flip();
+            buffer.flip();
+            while (buf.hasRemaining()) {
+                System.out.print((char) buf.get());
+            }
+            buf.clear();
+            buffer.clear();
+            read = channel.read(bufferArray);
+        }
+        randomAccessFile.close();
+    }
+}
---
+++ src/main/java/netty/java/nio/file/FileChannelTestTwo.java
@@ -0,0 +1,26 @@
+package netty.java.nio.file;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author crazy
+ * @title: FileChannelTestTwo
+ * @projectName JavaCode
+ * @description: 管道的双详情
+ * @date 2020/6/3017:36
+ */
+public class FileChannelTestTwo {
+
+    public static void main(String[] args) throws Exception {
+        RandomAccessFile fromFile = new RandomAccessFile("/Users/crazy/Downloads/fromFile.txt", "rw");
+        FileChannel fromChannel = fromFile.getChannel();
+        RandomAccessFile toFile = new RandomAccessFile("/Users/crazy/Downloads/toFile.txt", "rw");
+        FileChannel toChannel = toFile.getChannel();
+        long position = 0;
+        long count = fromChannel.size();
+        toChannel.transferFrom(fromChannel, position, count);
+
+    }
+}
